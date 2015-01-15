45757b12284a07915818a4abaf73af17c3b634f4
David Taylor <taylor.david.ray@gmail.com>
2015-01-14 16:17:45 -0800

    Refactored code to use accessors and hide data structure using methods

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,20 +5,33 @@
 #
 # Board class maintains data structure for tic-tac-toe
 # and provides interface for Game class
+#
+# Refactoring consisted of implementing accessors
+# Refactoring consisted of abstracting hash data structure with wrapper methods
+#
+# how do i make accessors private?
+#
 class Board
   
+  private
+  attr_accessor :occupied_spaces, :board
+
+  public
+
   OPEN = "_"
+
 
   def initialize
   	@occupied_spaces = 0
     @board = Hash.new(:invalid)
-    (1..3).each { |row| (1..3).each { |col| @board[:row => row, :col => col] = OPEN } }
+
+    (1..3).each { |row| (1..3).each { |col| set_cell(row, col, OPEN) } }
   end
 
   def display
   	printf "\n"
     (1..3).each do |row| 
-    	(1..3).each { |col| printf "#{@board[:row => row, :col => col]} " }
+    	(1..3).each { |col| printf "#{get_cell(row,col)} " }
     	printf "\n"
     end
     puts
@@ -26,8 +39,10 @@ class Board
 
   def mark(row, col, piece)
     if valid?(row,col)
-      @board[:row => row, :col => col] = piece
-      @occupied_spaces += 1
+      set_cell(row, col, piece)
+      #self.occupied_spaces += 1 # must use self for setter if occupied_spaces is public?, but this generates error when private
+      self.occupied_spaces = occupied_spaces + 1 # the getter can't use the self reference
+      #occupied_spaces=(occupied_spaces() + 1) # but now since it is private, it looks like this. wow. this is ugly!!
       true
     else
       false
@@ -35,51 +50,69 @@ class Board
   end
 
   def valid?(row, col)
-    (@board[:row => row, :col => col] == OPEN)    
+    equal_cell?( row, col, OPEN)
   end
 
   def full?
-    (@occupied_spaces >= 9)  
+    occupied_spaces >= 9
   end
 
   def winner?(piece)
     # iterate through rows
     (1..3).each do |row| 
     	match = true
-    	(1..3).each { |col| match=false unless @board[:row => row, :col => col] == piece }
+    	(1..3).each { |col| match=false unless equal_cell?(row, col, piece) }
     	return true if match == true
     end
     # iterate through columns
     (1..3).each do |col| 
     	match = true
-    	(1..3).each { |row| match=false unless @board[:row => row, :col => col] == piece }
+    	(1..3).each { |row| match=false unless equal_cell?(row, col, piece) }
     	return true if match == true
     end
     # check downward diagonal
     match = true
      (1..3).each do |row|
         col = row 
-    	match=false unless @board[:row => row, :col => col] == piece
+    	match=false unless equal_cell?(row, col, piece)
     end   
     return true if match == true
     # check upward diagonal
     match = true
      (1..3).each do |row|
         col = 4-row 
-    	match=false unless @board[:row => row, :col => col] == piece
+    	match=false unless equal_cell?(row, col, piece)
     end   
     return true if match == true    
     # if we get here, then there is no three in a row of piece
     false
   end
 
+  private
+
+  # note that I was unable to get set_cell= as a method name to work the same way as set_cell does
+  # abstract data structure for board
+  def set_cell(row, col, contents)
+    board[:row => row, :col => col] = contents
+  end
+
+  def get_cell(row, col)
+    board[:row => row, :col => col]
+  end
+
+  def equal_cell?(row, col, contents)
+    return true if get_cell(row, col) == contents
+    return false
+  end
 
 end
 
-#game class which keeps track of whose turn it is and does an infinite loop.
+#game class which keeps track of whose turn it is and does the game_over loop.
 #  displays messages
 #  determines who is the winner
 class Game
+
+  attr_accessor :player1, :player2, :board
 
   def initialize
     puts "Welcome to Tic-Tac-Toe\n"
@@ -91,7 +124,7 @@ class Game
   def get_input(player)
     puts "#{player}'s turn. Enter row, col to mark an #{player}"    
     row, col = gets.chomp.split(",").map { |i| i.to_i }
-      while !@board.valid?(row,col)
+      while !board.valid?(row,col)
       	puts "Invalid entry. Please retry."
         puts "#{player}'s turn. Enter row, col to mark an #{player}"    
         row, col = gets.chomp.split(",").map { |i| i.to_i }      	
@@ -100,19 +133,19 @@ class Game
   end
 
   def next_player(player)
-    (player == @player1) ? @player2 : @player1
+    (player == player1) ? player2 : player1
   end
 
   def play
     game_won  = false
-    current_player = @player1    
-    @board.display
+    current_player = player1    
+    board.display
 
     while !game_won && !@board.full?
       row, col = get_input(current_player)
-      @board.mark(row, col, current_player)
-      @board.display
-      game_won = @board.winner?(current_player)
+      board.mark(row, col, current_player)
+      board.display
+      game_won = board.winner?(current_player)
       current_player = next_player(current_player) unless game_won
     end # while !game_over
 
